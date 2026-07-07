91782a75a45757432e67a84bce1352ae3ab5c299
shifa <shifayasmin23@gmail.com>
2026-07-07 12:45:13 +0530

    switch

--- text.js
+++ text.js
@@ -12,9 +12,65 @@
 // }
 // console.log(a)
 
-function main(){
-    const a=10;
-    console.log(a)
+// function main(){
+//     const a=10;
+//     console.log(a)
+// }
+// main();
+// // console.log(a)
+
+// 
+// 
+
+// let a="shifa";
+// let b=100;
+// let c=a+b;
+// console.log(c);
+// console.log(typeof c)
+
+
+
+
+// let a=undefined;
+// let b=2;
+// let c=a/b;
+// console.log(c);
+// console.log(typeof c)
+
+
+// let a=undefinde;
+// console.log(Number(a));
+// console.log(Boolean("9"))
+
+
+// let a=19;
+// if(a<=18){
+//     console.log("small")
+// }else{
+//     console.log("big")
+// }
+
+// let age=15;
+// let isId=false;
+// if(age>=18){
+//    if(isId){
+//      console.log("eligible")
+//    }
+//       console.log("not eliglble")
+// }else{
+//     console.log("age is not")
+// }
+
+
+
+let day=1;
+switch(day){
+    case 1 :
+        console.log("sunday");
+        break;
+        case 2:
+            console.log("monday");
+            break;
+            default:
+             console.log("invalide")
 }
-main();
-// console.log(a)
