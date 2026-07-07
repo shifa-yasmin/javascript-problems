90e0450f009ed96edf1a6580c40431bc2138f930
shifa <shifayasmin23@gmail.com>
2026-07-07 10:07:53 +0530

    html

--- text.js
+++ text.js
@@ -6,8 +6,15 @@
 // a=20;
 // console.log(a)
 
-{
-    var a=10;
+// {
+//     var a=10;
+//     console.log(a)
+// }
+// console.log(a)
+
+function main(){
+    const a=10;
     console.log(a)
 }
-console.log(a)
+main();
+// console.log(a)
