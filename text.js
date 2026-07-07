552aa6613f5541e2f5d6796eea0c7cf2519f04b1
shifa <shifayasmin23@gmail.com>
2026-07-07 21:08:58 +0530

    multiple

--- text.js
+++ text.js
@@ -76,6 +76,88 @@
 // }
 
 
-let age=19;
-let res=(age>=18)?"adult":"minor";
-console.log(res)
+// let age=19;
+// let res=(age>=18)?"adult":"minor";
+// console.log(res)
+
+
+
+// let a=10;
+// let res=(a%2===0)?"even":"odd";
+// console.log(res)
+
+
+
+// let i=1;
+// while(i<=14){
+//      console.log(i);
+//      i++;
+// }
+
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+
+// let students={
+//     name:"shifa",
+//     age:10,
+//     place:"koramkod"
+// }
+// for(let ch in students){
+//     console.log(ch+":"+students[ch])
+// }
+
+
+
+
+// let name="shifayasmin";
+// let res=name.lastIndexOf("s");
+// console.log(res)
+
+
+// let name="javascript";
+// let res=name.charAt(4);
+// console.log(res)
+
+
+
+// let name="i love java";
+// let res=name.replace("java","javascript");
+// console.log(res)
+
+
+
+// let text = "Java JAVA java";
+// let res=text.replaceAll(/java/gi ,"javascript");
+// console.log(res)
+
+
+// let name="5";
+// let res=name.padStart(3,"0");
+// console.log(res)
+
+
+
+// let str = "abc45xyz";
+
+// console.log(str.search(/\d/));   // 3 (first digit: 4)
+// console.log(str.search(/[a-z]/)); // 0 (first lowercase letter: a)
+// console.log(str.search(/[A-Z]/)); // -1 (no uppercase letters)
+// console.log(str.search(/\s/));    // -1 (no spaces)
+
+
+
+
+// let res = "hello word";
+// console.log(res.startsWith("hello1"));
+
+// let text = "Hello World";
+// console.log(text.match("World"));
+
+
+let a=[1,2,3,4,4,6];
+console.log(a.map((n)=>n*2))
