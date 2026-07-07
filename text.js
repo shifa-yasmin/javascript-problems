3067d1ce1fcf73ea90362e62595098b0b25c3721
shifa <shifayasmin23@gmail.com>
2026-07-07 09:47:31 +0530

    block

--- text.js
+++ text.js
@@ -1,7 +1,13 @@
-let a=10;
-a=20;
-console.log(a)
+// let a=10;
+// a=20;
+// console.log(a)
 
-var a=10;
-a=20;
+// var a=10;
+// a=20;
+// console.log(a)
+
+{
+    var a=10;
+    console.log(a)
+}
 console.log(a)
